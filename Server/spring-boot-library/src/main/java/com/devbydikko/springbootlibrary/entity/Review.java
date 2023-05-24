6b363d27815f3a4f38305359a463458214a9b5b1
Bright Dikko <bdikko@nd.edu>
2023-05-23 17:50:09 -0700

    push db script

---
+++ Server/spring-boot-library/src/main/java/com/devbydikko/springbootlibrary/entity/Review.java
@@ -0,0 +1,12 @@
+package com.devbydikko.springbootlibrary.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="review")
+
+public class Review {
+}
