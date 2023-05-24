784b35ef3a480008bddf4c82a73f6a361aa7b8d7
Bright Dikko <bdikko@nd.edu>
2023-05-23 18:02:09 -0700

    create review entity

--- Server/spring-boot-library/src/main/java/com/devbydikko/springbootlibrary/entity/Review.java
+++ Server/spring-boot-library/src/main/java/com/devbydikko/springbootlibrary/entity/Review.java
@@ -1,12 +1,32 @@
 package com.devbydikko.springbootlibrary.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
 
 @Entity
 @Data
 @Table(name="review")
-
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="user_email")
+    private String userEmail;
+
+    @CreationTimestamp
+    @Column(name="date")
+    private Date date;
+
+    @Column(name="rating")
+    private double rating;
+
+    @Column(name="book_id")
+    private Long bookId;
+
+    @Column(name="review_description")
+    private String reviewDescription;
 }
