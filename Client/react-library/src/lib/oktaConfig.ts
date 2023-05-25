2ef476919de539d4aaac2cf90236b361ac6abb75
Bright Dikko <bdikko@nd.edu>
2023-05-24 20:42:51 -0700

    More setup

--- Client/react-library/src/lib/oktaConfig.ts
+++ Client/react-library/src/lib/oktaConfig.ts
@@ -1,6 +1,9 @@
+const CLIENT_ID = process.env.REACT_APP_OKTA_CLIENT_ID;
+const ISSUER = process.env.REACT_APP_OKTA_ISSUER;
+
 export const oktaConfig = {
-    clientId: "0oa9oxt2lwFfquJ6E5d7",
-    issuer: "https://dev-61508708.okta.com/oauth2/default",
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
     redirectUri: "http://localhost:3000/login/callback",
     scopes: ["openid", "profile", "email"],
     pkce: true,
