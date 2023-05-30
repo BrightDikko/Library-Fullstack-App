7491fffd67d4c7df722772e53668849306c54642
Bright Dikko <bdikko@nd.edu>
2023-05-29 19:01:16 -0700

    Server side functionality for Book Checkout

--- Server/spring-boot-library/src/main/java/com/devbydikko/springbootlibrary/config/SecurityConfiguration.java
+++ Server/spring-boot-library/src/main/java/com/devbydikko/springbootlibrary/config/SecurityConfiguration.java
@@ -4,12 +4,19 @@ import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,11 +24,29 @@ public class SecurityConfiguration {
         // Disable Cross Site Request Forgery
         http.csrf().disable();
 
-        // Protect endpoints at /api/<type>/secure
-        // This lines (22 to 24) are what's giving the issue
         http.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/books/secure/**").authenticated())
+                        .requestMatchers(request -> request.getServletPath().startsWith("/api/books/secure/"),
+                                request -> request.getServletPath().startsWith("/api/reviews/secure/"),
+                                request -> request.getServletPath().startsWith("/api/messages/secure/"),
+                                request -> request.getServletPath().startsWith("/api/admin/secure/"))
+                        .authenticated()
+                        .anyRequest().permitAll())
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        /*
+        // CORS configuration
+        http.cors(cors -> cors
+                .configurationSource(request -> {
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:3000"));
+                    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(List.of("*"));
+                    source.registerCorsConfiguration("/**", config);
+                    return config;
+                }));
+         */
 
         // Add CORS filter
         http.cors();
